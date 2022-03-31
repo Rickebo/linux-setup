1ccd9b78ce7629099fa268259e3e96bd40b6f66a
Simon Pettersson <rickebo@rickebo.com>
2022-03-31 13:56:06 +0200

    Fixed syntax error

--- setup-remote.sh
+++ setup-remote.sh
@@ -6,6 +6,7 @@ REQUIRED_COMMANDS=(sudo git curl)
 
 for required in ${REQUIRED_COMMANDS[@]}; do
     if ! command -v $required &> /dev/null
+    then
         echo "Installing $required..."
         apt install $required -y
     fi
