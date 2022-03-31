377932d1e0e429219da694dcd6454b3ada8b0d26
root <root@minecrafters.se>
2022-03-31 10:51:27 +0000

    Added remote script

---
+++ setup-remote.sh
@@ -0,0 +1,20 @@
+REPO_NAME=linux-setup
+REPO=https://github.com/Rickebo/$REPO_NAME.git
+STARTUP_SCRIPT=setup.sh
+
+REQUIRED_COMMANDS=(sudo git curl)
+
+for required in ${REQUIRED_COMMANDS[@]}; do
+    if ! command -v $required &> /dev/null
+        echo "Installing $required..."
+        apt install $required -y
+    fi
+done
+
+git clone $REPO
+
+if [ "$EUID" -ne 0 ]
+    sudo ./$REPO/$STARTUP_SCRIPT
+else
+    ./$REPO/$STARTUP_SCRIPT
+fi
